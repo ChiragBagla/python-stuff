764d4392cba44fc6d1904caa0ac5ba2764bda3a0
ChiragBagla <005chirag@gmail.com>
2016-02-08 20:36:25 +0530

    1st commit

---
+++ hackerrank/intro/i10.py
@@ -0,0 +1,7 @@
+a = int(raw_input())
+b = int(raw_input())
+
+c = (b,a)
+
+print c[0]
+print c[1]
---
+++ hackerrank/intro/i7.py
@@ -0,0 +1,9 @@
+a = int(raw_input())
+b = int(raw_input())
+c = int(raw_input())
+d = int(raw_input())
+
+x = pow(a,b)
+y = pow(c,d)
+
+print x+y
